da164c937ab52c82ef7da7fcff6c1bd96beba32d
Evgenii Buslaev <eugenebus144@gmail.com>
2022-11-03 10:23:49 +0500

    added components and styles according to course template

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="wrap">
+        <div className="card">
+          <div className="card-image">
+            <img src="./sea.jpg" alt="surfing" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
