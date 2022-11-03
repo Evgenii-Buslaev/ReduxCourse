3371c65443a652f370ae944681481d55d511553a
Evgenii-Buslaev <eugenebus144@gmail.com>
2022-11-03 14:29:47 +0500

    Refactor: created rootReducer, types, likeReducer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createStore } from "redux";
+import { rootReducer } from "./redux/rootReducer";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 
+const store = createStore(rootReducer);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
